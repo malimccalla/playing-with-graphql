4c62cb9b9a437be356bc0675b46f16287055c2b6
Mali Michael <malimccalla@gmail.com>
2017-08-12 12:20:57 +0100

    Adds company to root query

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,30 @@ const axios = require('axios');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `http://localhost:3000/companies/${parentValue.companyId}`
+        );
+        return data;
+      }
+    }
   }
 });
 
@@ -21,6 +39,16 @@ const RootQuery = new GraphQLObjectType({
       async resolve(parentValue, args) {
         const { data } = await axios.get(
           `http://localhost:3000/users/${args.id}`
+        );
+        return data;
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(
+          `http://localhost:3000/companies/${args.id}`
         );
         return data;
       }
